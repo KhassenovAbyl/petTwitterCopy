aa74f1dd0321eee84d5f08edff33798eb4ac77c5
Abyl <khassenov00@icloud.com>
2020-11-26 15:55:21 +0600

    Initial Commit

---
+++ Twitter/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Twitter
+//
+//  Created by Abylbek Khassenov on 11/26/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
